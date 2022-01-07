59e7d6b7465751849d9efcf0922a55cdc505cd4a
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2022-01-07 11:52:13 -0300

    AnalyticsFacade now used in getData (testing wip)

--- src/AnalyticsFacade.php
+++ src/AnalyticsFacade.php
@@ -8,4 +8,24 @@ class AnalyticsFacade extends \Illuminate\Support\Facades\Facade
     {
         return "Analytics";
     }
+
+    /**
+     * Handle dynamic, static calls to the object.
+     *
+     * @param  string  $method
+     * @param  array  $args
+     * @return mixed
+     *
+     * @throws \RuntimeException
+     */
+    public static function __callStatic($method, $args)
+    {
+        $instance = static::getFacadeRoot();
+
+        if (! $instance) {
+            throw new RuntimeException('A facade root has not been set.');
+        }
+
+        return $instance->$method(...$args);
+    }
 }
