881800b0ff8aba5243ef1af9fdbd1f77bc5aa582
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2022-05-27 03:21:31 -0300

    Two Axis Ranges Formatter Added (to be used with heatmap widget)

---
+++ src/Http/Controllers/Dashboard/Formaters/TwoAxisRangesFormatter.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Asivas\Analytics\Http\Controllers\Dashboard\Formaters;
+
+
+use Illuminate\Support\Carbon;
+
+class TwoAxisRangesFormatter implements WidgetFormatter
+{
+
+    static function formatResponse(Carbon $dFrom, Carbon $dTo, array $allSeries, array $compareSeries = null): array
+    {
+        $ranges= [];
+        $computedSeries = [];
+        foreach ($allSeries as $serie) {
+            $computedSerie = [];
+            $computedSerie['name'] = $serie['name'];
+            $ranges = array_keys($serie['data']);
+            $computedSerie['data'] = array_values($serie['data']);
+            $computedSeries[] = $computedSerie;
+        }
+        return
+            [
+                "ranges" => $ranges,
+                "series" => $computedSeries
+            ];
+    }
+}
