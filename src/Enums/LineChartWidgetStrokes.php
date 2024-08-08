b7c45649ec5c7bab4a698af60a735919f4c724e3
Lucas Vidaguren <lucas.vidaguren@asivas.com.ar>
2024-08-07 21:22:25 -0300

    Add stroke to LineChart
    php 8.1+

---
+++ src/Enums/LineChartWidgetStrokes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Asivas\Analytics\Enums;
+
+enum LineChartWidgetStrokes:string
+{
+    case straight='straight';
+    case spline='spline';
+    case stepline='stepline';
+}
--- src/Widget/LineChartWidget.php
+++ src/Widget/LineChartWidget.php
@@ -2,13 +2,40 @@
 
 namespace Asivas\Analytics\Widget;
 
+use Asivas\Analytics\Enums\LineChartWidgetStrokes;
 use Asivas\Analytics\Http\Controllers\Dashboard\LineChartController;
 
 class LineChartWidget extends Widget
 {
+    protected $stroke = LineChartWidgetStrokes::straight;
+
     public function __construct($title)
     {
         parent::__construct($title, 'LineChart', LineChartController::class);
+    }
+
+    public function getStroke(): string
+    {
+        return $this->stroke;
+    }
+
+    /**
+     * @param LineChartWidgetStrokes $stroke
+     * @return void
+     */
+    public function setStroke(LineChartWidgetStrokes $stroke): self
+    {
+        $this->stroke = $stroke;
+        return $this;
+    }
+
+
+    public function toArray(): array
+    {
+        return parent::toArray() +
+            [
+                'stroke' => $this->stroke
+            ];
     }
 
 }
